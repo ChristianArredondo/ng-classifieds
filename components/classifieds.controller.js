e7a69d433764550bd37ea4d3628251386d746ab7
christianarredondo <christianarredondo@utexas.edu>
2018-07-01 19:18:58 -0500

    add sidenav with form

--- components/classifieds.controller.js
+++ components/classifieds.controller.js
@@ -3,11 +3,19 @@
 
   angular
     .module('ngClassifieds')
-    .controller('classifiedsCtrl', ($scope, $http, classifiedsFactory) => {
+    .controller('classifiedsCtrl', ($scope, $http, classifiedsFactory, $mdSidenav) => {
 
       classifiedsFactory.getClassifieds().then(classifieds => {
         $scope.classifieds = classifieds.data;
       });
+
+      $scope.openSidebar = () => {
+        $mdSidenav('left').open();
+      }
+
+      $scope.closeSidebar = () => {
+        $mdSidenav('left').close();
+      }
 
     });
 })();

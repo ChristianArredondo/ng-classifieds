d5a7c56b75929d51790ed89bfefd4819f4c13194
christianarredondo <christianarredondo@utexas.edu>
2018-07-01 20:24:38 -0500

    add method for adding new classified (locally) and showing mdToast

--- components/classifieds.controller.js
+++ components/classifieds.controller.js
@@ -3,7 +3,13 @@
 
   angular
     .module('ngClassifieds')
-    .controller('classifiedsCtrl', ($scope, $http, classifiedsFactory, $mdSidenav) => {
+    .controller('classifiedsCtrl', ($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) => {
+
+      const contact = {
+        name: 'Christian',
+        phone: '(888) 888-8888',
+        email: 'rofl@test.com'
+      };
 
       classifiedsFactory.getClassifieds().then(classifieds => {
         $scope.classifieds = classifieds.data;
@@ -15,6 +21,23 @@
 
       $scope.closeSidebar = () => {
         $mdSidenav('left').close();
+      }
+
+      $scope.saveClassified = (classified) => {
+        if (classified) {
+          $scope.classifieds.push({
+            contact,
+            ...classified,
+          });
+          $scope.closeSidebar();
+          $scope.classified = {};
+          $mdToast.show(
+            $mdToast.simple()
+              .content('Classified saved!')
+              .position('top, right')
+              .hideDelay(3000)
+          );
+        }
       }
 
     });

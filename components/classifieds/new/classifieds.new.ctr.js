fcf24f254c16bb46ee30c93098b482cda063d1b1
christianarredondo <christianarredondo@utexas.edu>
2018-07-11 22:31:18 -0500

    add $watch to sidenav for re-routing

--- components/classifieds/new/classifieds.new.ctr.js
+++ components/classifieds/new/classifieds.new.ctr.js
@@ -2,11 +2,19 @@
   'use strict';
   angular
     .module('ngClassifieds')
-    .controller('newClassifiedsCtrl', function($mdSidenav, $timeout, $mdDialog, classifiedsFactory) {
+    .controller('newClassifiedsCtrl', function($scope, $state, $mdSidenav, $timeout, $mdDialog, classifiedsFactory) {
       const vm = this;
 
       $timeout(function() {
         $mdSidenav('left').open();
+      });
+
+      $scope.$watch('vm.sidenavOpen', function(value) {
+        if (value === false) {
+          $mdSidenav('left').close().then(function() {
+            $state.go('classifieds');
+          })
+        }
       });
     })
 })();

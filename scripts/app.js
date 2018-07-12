b03aa49f3afad43160cf313b2a7979932ba2dba5
christianarredondo <christianarredondo@utexas.edu>
2018-07-11 20:37:53 -0500

    begin ui-routing

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,31 @@
 angular
-  .module('ngClassifieds', ['ngMaterial'])
-  .config(($mdThemingProvider) => {
+  .module('ngClassifieds', ['ngMaterial', 'ui.router'])
+  .config(($mdThemingProvider, $stateProvider) => {
     $mdThemingProvider.theme('default')
       .primaryPalette('teal')
       .accentPalette('orange');
+
+    $stateProvider
+      .state('one', {
+        url: '/stateone',
+        template: '<h1>{{ stateOne.message }}</h1>',
+        controller: 'stateOneCtrl as stateOne'
+      })
+      .state('two', {
+        url: '/statetwo',
+        template: '<h1>State Two</h1> <md-button ui-sref="two.more">Nested state</md-button> <ui-view></ui-view>'
+      })
+      .state('two.more', {
+        url: '/more',
+        template: '<p>Deeper state</p>'
+      })
   })
   .directive('helloWorld', () => {
     return {
       template: '<h1>Hello, world!</h1>'
     }
   })
+  .controller('stateOneCtrl', function() {
+    const vm = this;
+    vm.message = 'Hello from state one';
+  });
